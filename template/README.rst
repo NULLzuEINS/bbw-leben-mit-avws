674c3eed98f6a2c5405eb5a5fbdfa2a57495244f
André Lademann <vergissberlin@googlemail.com>
2018-06-05 21:43:42 +0200

    Added template files

---
+++ template/README.rst
@@ -0,0 +1,6 @@
+==================================================
+Template Package
+==================================================
+
+Template Package delivers a full configured frontend
+theme for TYPO3, based on the TYPO3 Bootstrap Package.
---
+++ template/ext_emconf.php
@@ -0,0 +1,41 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "template_package".
+ *
+ * Auto generated 27-05-2018 21:36
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Template Package',
+  'description' => 'Template package based on TYPO§ Bootstrap Package',
+  'category' => 'templates',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '8.7.0-9.0.99',
+      'bootstrap_package' => '8.0.0-9.0.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'state' => 'stable',
+  'uploadfolder' => true,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 1,
+  'author' => 'André Lademann',
+  'author_email' => 'info@nullzueins.com',
+  'author_company' => 'private',
+  'version' => '1.0.0',
+  'clearcacheonload' => false,
+);
+
