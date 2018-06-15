bf8af854727875b96ae711be2fc8d0d4ca89f8f0
André Lademann <vergissberlin@googlemail.com>
2018-06-15 22:12:58 +0200

    Improve code style

--- template/ext_emconf.php
+++ template/ext_emconf.php
@@ -10,32 +10,32 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
-  'title' => 'Template - Leben mit AVWS',
-  'description' => 'Template package based on TYPO3 Bootstrap Package',
-  'category' => 'templates',
-  'constraints' => 
-  array (
-    'depends' => 
-    array (
-      'typo3' => '8.7.0-9.0.99',
-      'bootstrap_package' => '8.0.0-9.2.99',
-    ),
-    'conflicts' => 
-    array (
-    ),
-    'suggests' => 
-    array (
-    ),
-  ),
-  'state' => 'stable',
-  'uploadfolder' => true,
-  'createDirs' => '',
-  'clearCacheOnLoad' => 1,
-  'author' => 'André Lademann',
-  'author_email' => 'info@nullzueins.com',
-  'author_company' => 'private',
-  'version' => '1.0.0',
-  'clearcacheonload' => false,
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Template - Leben mit AVWS',
+    'description' => 'Template package based on TYPO3 Bootstrap Package',
+    'category' => 'templates',
+    'constraints' =>
+        [
+            'depends' => [
+                'typo3' => '8.7.0-9.5.99',
+                'rte_ckeditor' => '8.7.0-9.5.99',
+                'bootstrap_package' => '9.1.0-9.1.99'
+            ],
+            'conflicts' =>
+                [
+                ],
+            'suggests' =>
+                [
+                ],
+        ],
+    'state' => 'stable',
+    'uploadfolder' => true,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'André Lademann',
+    'author_email' => 'info@nullzueins.com',
+    'author_company' => 'NULLzuEINS Inh. André Lademann',
+    'version' => '1.0.0',
+    'clearcacheonload' => false,
+];
 
